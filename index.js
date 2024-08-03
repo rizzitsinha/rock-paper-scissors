571b7cc0b9ec676ee1fa2e1e037ab55b64616123
Rishit Sinha <rishitsinha07@gmail.com>
2024-08-04 01:23:17 +0530

    Edit index.js
    Add comment for function getComputerChoice

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// Create a funciton called getComputerChoice to generate a random choice for the computer to play, either rock, paper or scissors
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
     let playableOptions = ['rock', 'paper', 'scissors'];
     return playableOptions[randomNumber];
 }
+
