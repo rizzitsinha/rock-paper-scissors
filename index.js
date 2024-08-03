d5ae02845efa005bfffe8514d974961f9dbaa3c9
Rishit Sinha <rishitsinha07@gmail.com>
2024-08-04 01:34:51 +0530

    Edit index.js
    Create function to take user input and return it in uniform manner

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
     let playableOptions = ['rock', 'paper', 'scissors'];
+    
     return playableOptions[randomNumber];
 }
 
+// Create a function called getHumanChoice which will return one of the valid options, depending on the user's input
+function getHumanChoice(){
+    let humanChoice = prompt("Play either rock, paper or scissors please!");
+    humanChoice = humanChoice.toLowerCase();
+    
+    return humanChoice;
+}
