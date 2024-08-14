cf83a00daa97d10b1f570fdd6854b9c744eac5ef
Rishit Sinha <rishitsinha07@gmail.com>
2024-08-15 00:53:54 +0530

    Site works but however winner status isnt displayed

--- index.js
+++ index.js
@@ -2,30 +2,42 @@ const rockBtn = document.querySelector("#rock")
 const paperBtn = document.querySelector("#paper")
 const scissorsBtn = document.querySelector("#scissors")
 const buttonsContainer = document.querySelector(".buttons-container")
+const currentScore = document.querySelector("#score");
 
 // Creat two variables humanScore and computerScore and initialize the two to 0
 let humanScore = 0;
 let computerScore = 0;
 let humanChoice;
 
-// // Initiate Game
-// buttonsContainer.addEventListener('click', (e) => {
-//     let target = e.target;
-//     switch(target.id){
-//         case 'rock':
-//             console.log("rock was clicked!");
-//             humanChoice = 'rock';
-//             break;
-//         case 'paper':
-//             console.log("paper was clicked!");
-//             humanChoice = 'paper';
-//             break;
-//         case 'scissors':
-//             console.log("scissors was clicked!");
-//             humanChoice = 'scissors';
-//             break;
-//     }
-// })
+// Initiate Game
+buttonsContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    switch(target.id){
+        case 'rock':
+            console.log("rock was clicked!");
+            humanChoice = 'rock';
+            playRound(humanChoice, getComputerChoice());
+            currentScore.textContent = `${humanScore} - ${computerScore}`
+
+            break;
+        case 'paper':
+            console.log("paper was clicked!");
+            humanChoice = 'paper';
+            playRound(humanChoice, getComputerChoice());
+            currentScore.textContent = `${humanScore} - ${computerScore}`
+
+            break;
+        case 'scissors':
+            console.log("scissors was clicked!");
+            humanChoice = 'scissors';
+            playRound(humanChoice, getComputerChoice());
+            currentScore.textContent = `${humanScore} - ${computerScore}`
+
+            break;
+    }
+})
+
+
 
 
 // Create a funciton called getComputerChoice to generate a random choice for the computer to play, either rock, paper or scissors
@@ -36,13 +48,6 @@ function getComputerChoice(){
     return playableOptions[randomNumber];
 }
 
-// Create a function called getHumanChoice which will return one of the valid options, depending on the user's input
-function getHumanChoice(){
-    let humanChoice = prompt("Play either rock, paper or scissors please!");
-    humanChoice = humanChoice.toLowerCase();
-    
-    return humanChoice;
-}
 
 // Create a function called playRound to play a single round, decide and declare the winner
 function playRound(humanChoice, computerChoice){
@@ -73,15 +78,5 @@ function playRound(humanChoice, computerChoice){
     console.log(result);
 }
 
-// Call the until either the user or the compuer achieves 5 points
-while(humanScore < 5 && computerScore < 5) {
-    playRound(getHumanChoice(), getComputerChoice());
-}
 
-// Announce the final result and declare the winner
-console.log(`Final Score:
-                   Your Score- ${humanScore}
-                   Computer's Score- ${computerScore}`)
-// if (humanScore > computerScore){ console.log("You win!!!!") }
-// else if (humanScore < computerScore){ console.log("You lose:(") }
-// else { console.log("It's a tie") }
+
